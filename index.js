dc2cc17c7fb9b1963b90a28185e056c7c6789f03
sabbir <sabbir@gmail.com>
2023-03-26 15:33:53 +0600

    implement jwt

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require("stripe")('sk_test_51M8kbEBhLbcvUmB2stvcZFbvjerpFRcPixbzcyHr9WGntnlVfyGrCU1jLDkfhxMUWD3AwVLK4VnvJjnB2eevV6OI00lDNBHDNU');
 
 // midleware
 app.use(cors());
@@ -19,11 +21,39 @@ app.get('/', (req, res) => {
     res.send('hellow world');
 });
 
+const verfyJWT = (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: "anathorizrd access" });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "anathorizrd access" });
+        }
+
+        req.decoded = decoded;
+        next();
+    });
+
+};
+
 async function run() {
     try {
         const Products = client.db('resalePhone').collection('products');
         const Users = client.db('resalePhone').collection('users');
         const Orders = client.db('resalePhone').collection('orders');
+        const Payments = client.db('resalePhone').collection('payments');
+
+
+        app.use('/jwt', async (req, res) => {
+            const email = req.body.email;
+            const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, { expiresIn: '1hr' });
+            res.send({ token: token });
+        });
+
 
 
         app.get('/categoryProducts/:id', async (req, res) => {
@@ -85,7 +115,12 @@ async function run() {
         });
 
         //get buyer order
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verfyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                return res.status(401).send({ message: "anathorizrd access" });
+            }
             const email = req.query.email;
             const query = { email };
             const result = await Orders.find(query).toArray();
@@ -101,7 +136,7 @@ async function run() {
         });
 
         //delete a order
-        app.get('/booking/:id', async (req, res) => {
+        app.get('/booking/:id', verfyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await Orders.findOne(query);
@@ -147,7 +182,12 @@ async function run() {
 
 
         //get seller product
-        app.get('/product', async (req, res) => {
+        app.get('/product', verfyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                return res.status(401).send({ message: "anathorizrd access" });
+            }
             const email = req.query.email;
             const query = { email };
             const result = await Products.find(query).toArray();
@@ -161,6 +201,42 @@ async function run() {
             const result = await Products.deleteOne(query);
             res.send(result);
         });
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const price = req.body.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await Payments.insertOne(payment);
+            const orderdID = payment.bookingId;
+            const filter1 = { _id: new ObjectId(orderdID) };
+            // const id = payment.orderId;
+            // const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: "paid",
+                    transactionId: payment.transactionId
+                }
+            };
+            // const updatedResult = await Orders.updateOne(filter, updatedDoc);
+            const updatedResult1 = await Orders.updateOne(filter1, updatedDoc);
+            res.send(updatedResult1);
+        });
+
 
 
     }
